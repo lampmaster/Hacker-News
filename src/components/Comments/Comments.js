1bf992ea984d15334b2718c8482aa786113a953b
lamp <paperslink@gmail.com>
2020-11-30 04:46:25 +0300

    comments: fixed bugs

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,7 +2,24 @@ import React from 'react';
 import classes from './Comments.module.scss'
 import {getDate} from "../../common/utils";
 
-export const Comments = ({comments}) => {
+export const Comments = ({comments, openedReplies, onChange}) => {
+
+    console.log(openedReplies);
+
+    const openReplies = (commentId) => {
+        if (openedReplies[commentId]) {
+            delete openedReplies[commentId];
+        } else {
+            openedReplies[commentId] = {};
+        }
+        onChange(openedReplies)
+    };
+
+    const handleSubReplies = (optionId, subSelections) => {
+        openedReplies[optionId] = subSelections;
+        onChange(openedReplies);
+    };
+
     return (
         <React.Fragment>
             {
@@ -13,11 +30,16 @@ export const Comments = ({comments}) => {
                             <div className={classes.text}>{comment.text}</div>
                             <div className={classes.bottom}>
                                 <div className={classes.time}>{getDate(comment.time)}</div>
-                                <div className={classes.replies}>{comment.childComentsNumber ? `${comment.childComentsNumber} replies` : ''}</div>
+                                <div onClick={() => {openReplies(comment.id)}} className={classes.replies}>{comment.childComentsNumber ? `${comment.childComentsNumber} replies` : ''}</div>
                             </div>
                             <div className={classes.container}>
                                 {
-                                    comment.kids && <Comments comments={comment.kids}/>
+                                    comment.kids && openedReplies[comment.id] &&
+                                    <Comments
+                                        comments={comment.kids}
+                                        openedReplies={openedReplies[comment.id]}
+                                        onChange={(subSelection) => handleSubReplies(comment.id, subSelection)}
+                                    />
                                 }
                             </div>
                         </div>
